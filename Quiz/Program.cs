1d7ac421a1ac230dba706a7f5d4750c611da1280
aherman-san <aherman@op.pl>
2025-11-16 09:26:32 +0100

    2025-11-16

---
+++ Quiz/Program.cs
@@ -0,0 +1,26 @@
+﻿// Powitanie
+//Console.WriteLine();
+//Console.WriteLine(" Witaj w aplikacji Quiz");
+//Console.WriteLine(" Spróbuj oddpowiedzieć na 7 pytań");
+//Console.WriteLine(" Powodzenia !!!");
+//Console.WriteLine();
+//Console.Write(" Naciśnij dowolny klawisz, aby rozpoczać grę ...");
+//Console.ReadLine();
+//Console.Clear();
+
+// Wyświetlenie pierwszego pytania
+//Console.WriteLine();
+//Console.WriteLine(" Pytanie za 100 pkt");
+
+using Quiz;
+
+Console.WriteLine();
+int x = 5;
+bool y = true;
+string z = "Ala ma kota";
+Question pytanie1;
+Question pytanie2;
+Question pytanie3;
+Question pytanie4;
+var t = 45;
+
